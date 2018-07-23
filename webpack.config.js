5f97615ca3caa52ab5b72661316a7c91454cb59b
DevamChaturvedi <devamchaturvedi0112@gmail.com>
2018-07-23 09:11:45 +0000

    first

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: __dirname+"./public",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        
+        use:[ {
+           loader: 'style-loader',
+           
+          },
+          {
+            loader: 'css-loader'
+         }]
+       },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use:{
+           loader: 'sass-loader',
+           options: {
+              presets: ['env']
+            }
+        }
+      }
+    ]
+  },
+  plugins: [
+  new HtmlWebpackPlugin({template: 'index.html'})
+  ]
+} 
